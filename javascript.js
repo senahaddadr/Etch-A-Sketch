abbd46b89801628dda3af5a132a711d31ef3e372
sina <srezaeihaddad@gmail.com>
2022-11-23 00:50:38 -0500

    Fix Grid Layout

--- javascript.js
+++ javascript.js
@@ -3,12 +3,14 @@ let grid = [];
 
 function setDiv() {
     let input = window.prompt('How many squares per side?')
-    grid = [];
+    container.textContent = "";
     for (i=1; i<=input**2; i++) {
-        grid[i] = document.createElement('div');
-        grid[i].classList.add('box')
-        container.append(grid[i]);
+        let box = document.createElement('div');
+        box.classList.add('box')
+        box.textContent= "s"
+        container.append(box);
     }
+    container.style.gridTemplate = `repeat(${input}, 1fr) / repeat(${input}, 1fr)`;
 }
 
 document.querySelector('#userInput').onclick = setDiv;
